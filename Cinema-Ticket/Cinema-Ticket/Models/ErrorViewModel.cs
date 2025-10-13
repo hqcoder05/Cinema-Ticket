7f3169f30dcabcc35e0d840d61367f8f8e4fafa8
hqcoder05 <hoangquocfw@gmail.com>
2025-10-13 15:33:17 +0700

    First commit

---
+++ Cinema-Ticket/Cinema-Ticket/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Cinema_Ticket.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
