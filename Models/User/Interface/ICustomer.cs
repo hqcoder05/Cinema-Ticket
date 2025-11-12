496db33a112630db7a202db5be077089f8ef4ca5
Quoc Hoang Ng <168975831+hqcoder05@users.noreply.github.com>
2025-11-12 18:36:24 +0700

    Dele

--- Models/User/Interface/ICustomer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cinema_ticket.Models.User.Interface
-{
-    public interface ICustomer : IUser
-    {
-        public void ViewBookingHistory();
-        public void BookTicket(int showtimeId, int seatId);
-        void CancelBooking(int bookingId);
-    }
-}
--- Models/User/UserRole.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Cinema_ticket.Models.User
-{
-    public enum UserRole
-    {
-        Admin = 0,
-        Customer = 1
-    }
-}
