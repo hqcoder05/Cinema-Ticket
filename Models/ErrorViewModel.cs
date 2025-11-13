a670609c7f4e51db5aa5cf310b3e4616023a6c5a
hqcoder05 <hoangquocfw@gmail.com>
2025-11-13 19:24:46 +0700

    delete

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cinema_ticket.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
