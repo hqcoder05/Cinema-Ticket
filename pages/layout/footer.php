e369c41c5a9c6106b44dd26e5fbba0bff6f74ef5
PhamVanTuan205 <vantuan.412005@gmail.com>
2025-11-14 22:44:59 +0700

    Commit

---
+++ pages/layout/footer.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="/">
+<div class="footer">
+    <div class="footer-left">
+        <h3>Thông Tin Liên Hệ</h3>
+        <p>Gmail: CGV@gmail.com</p>
+        <p>Hotline: 0802 115 543</p>
+        <p>Address: Tan Binh, Ho Chi Minh, Viet Nam</p>
+    </div>
+    <div class="footer-right">
+        <h3>Về chúng tôi</h3>
+        <p>CGV - Ứng dụng đặt vé xem phim tiện lợi, nhanh chóng và an toàn.</p>
+        <p>Trải nghiệm điện ảnh chỉ với vài cú chạm – mọi lúc, mọi nơi!</p>
+    </div>
+</div>
